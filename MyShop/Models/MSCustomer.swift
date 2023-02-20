e5f3f7f438f3b4983eabe13cfa0e171cf5af62b7
Sergey <bindasov.sergey@gmail.com>
2023-02-20 20:38:22 +0100

    added customer model

---
+++ MyShop/Models/MSCustomer.swift
@@ -0,0 +1,45 @@
+//
+//  MSCustomer.swift
+//  MyShop
+//
+//  Created by Sergey on 20.02.2023.
+//
+
+import Foundation
+
+struct MSCustomer: Codable {
+    let id: Int
+    let firstName: String
+    let lastName: String
+    let age: Int
+    let gender: String
+    let email: String
+    let phone: String
+    let username: String
+    let password: String
+    let image: String
+    let adress: [Adress]
+    let bank: [Bank]
+
+}
+
+struct Adress: Codable {
+    let adress: String
+    let city: String
+    let postalCode: String
+    let state: String
+    let coordinates: [Coordinates]
+}
+
+struct Coordinates: Codable {
+    let lat: Double
+    let lng: Double
+}
+
+struct Bank: Codable {
+    let cardExpire: String
+    let cardNumber: String
+    let cardType: String
+    let currency: String
+    let iban: String 
+}
